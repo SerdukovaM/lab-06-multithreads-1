e7d4e236ff840840cb88f402f46ac4a959c6b081
nishaque <61164463+nishaque@users.noreply.github.com>
2021-01-19 11:05:13 +0300

    Update worker.cpp

--- sources/worker.cpp
+++ sources/worker.cpp
@@ -3,37 +3,36 @@
 #ifndef TEMPLATE_WORKER_CPP
 #define TEMPLATE_WORKER_CPP
 
-#include <csignal>
-#include "a_sha_worker.cpp"
 #include <boost/log/core.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/console.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/console.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <csignal>
 
-
+#include "a_sha_worker.cpp"
 
 a_sha_worker* gp_worker;
 
 void handle(int s) {
-  if (s==SIGINT) {
+  if (s == SIGINT) {
     gp_worker->cancel();
   }
 }
 
-int main(int argc, char* argv[]) {
-  if ((argc!=2) and (argc!=3)) {
+int run(int, char**);
+
+int run(int argc, char* argv[]) {
+  if ((argc != 2) and (argc != 3)) {
     std::cout << "worker <json results path> <number of threads>";
     return 0;
   }
   boost::log::add_file_log(
-        boost::log::keywords::file_name = "async_worker_%5N.log",
-        boost::log::keywords::rotation_size = 10 * 1024 * 1024,
-        boost::log::keywords::format = "[%TimeStamp%][%Severity%]: %Message%"
-      );
-  boost::log::add_console_log(std::cout,
-        boost::log::keywords::format = "[%TimeStamp%][%Severity%]: %Message%"
-      );
+      boost::log::keywords::file_name = "async_worker_%5N.log",
+      boost::log::keywords::rotation_size = 10 * 1024 * 1024,
+      boost::log::keywords::format = "[%Severity%]: %Message%");
+  boost::log::add_console_log(
+      std::cout, boost::log::keywords::format = "[%Severity%]: %Message%");
   size_t n_zeros = 4;
   size_t n_threads = std::thread::hardware_concurrency();
   std::string json_path = argv[1];
