60f220ad2078fb9dc95de3d7c83308d1f47d427d
nishaque <61164463+nishaque@users.noreply.github.com>
2021-01-19 11:06:16 +0300

    Update a_sha_worker.hpp

--- include/a_sha_worker.hpp
+++ include/a_sha_worker.hpp
@@ -1,23 +1,23 @@
-//
-// Created by Юлия Тимошенко on 21.12.2020.
-//
+// Copyright 2020 Yulia Timoshenko <timoshenkojulie01@gmail.com>
 
-#ifndef TEMPLATE_A_SHA_WORKER_HPP
-#define TEMPLATE_A_SHA_WORKER_HPP
+#ifndef A_SHA_WORKER_HPP
+#define A_SHA_WORKER_HPP
 
-#include <string>
 #include <atomic>
-#include <thread>
 #include <mutex>
 #include <nlohmann/json.hpp>
+#include <string>
+#include <thread>
 
 class a_sha_worker {
  public:
-  a_sha_worker(size_t n_thr = std::thread::hardware_concurrency(), size_t N=4, bool autorun=true);
+  a_sha_worker(size_t n_thr = std::thread::hardware_concurrency(), size_t N = 4,
+               bool autorun = true);
   void start();
   void await();
   void cancel();
   void dump_json(const std::string&);
+
  private:
   std::vector<std::thread> pool{};
   size_t n_threads, n_zeros;
@@ -27,4 +27,4 @@ class a_sha_worker {
   std::atomic_bool cancelled;
 };
 
-#endif  // TEMPLATE_A_SHA_WORKER_HPP
+#endif  // A_SHA_WORKER_HPP
